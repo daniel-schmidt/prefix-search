653ac6a4ec4afeb6e7ace1517da25bdbc6820b34
Daniel Schmidt <daniel@neonfoto.de>
2022-07-09 19:21:02 +0200

    Implement useful tests for (de-)serialization and find, also use gmock now.

--- test/FindTest.cpp
+++ test/FindTest.cpp
@@ -1,8 +1,21 @@
-#include <gtest/gtest.h>
+#include "Find.h"
+#include "PrefixTree.h"
 
-TEST(FindTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <gmock/gmock.h>
+#include <vector>
+#include <string>
+
+using namespace TreeSearch;
+using ::testing::ContainerEq;
+
+TEST(FindTests, ReturnsMatchingSubtree) 
+{
+    std::vector<std::string> input{"Anananas", "Bratwurst", "Clementine", "Baum", "Bein", "Banane"};
+    std::string prefix = "Ba";
+    
+    NodeContainer const root = VecToTree(input, 1);
+    NodeContainer const result_subtree = Find(prefix, root, 1);
+    std::vector<std::string> result = TreeToVec(prefix, result_subtree);
+
+    EXPECT_THAT(result, ContainerEq(std::vector<std::string>{"Baum", "Banane"}));
 }
--- test/SerializeTests.cpp
+++ test/SerializeTests.cpp
@@ -1,10 +1,45 @@
 #include "PrefixTree.h"
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <vector>
 #include <string>
 
-TEST(SerializeTests, VecToTree) {
+using namespace TreeSearch;
+
+using ::testing::SizeIs;
+using ::testing::ContainerEq;
+
+
+TEST(SerializeTests, StoresSingleWord) 
+{
     std::vector<std::string> input{"Banane"};
-    TreeSearch::VecToTree(input, 1);
+    
+    NodeContainer const root = VecToTree(input, 1);
+    ASSERT_THAT(root, SizeIs(1));
+    EXPECT_EQ(root[0].content, 'B');
+    ASSERT_THAT(root[0].children, SizeIs(1));
+    EXPECT_EQ(root[0].children[0].content, 'a');
+
+    std::vector<std::string> result = TreeToVec("", root);
+    EXPECT_THAT(result, ContainerEq(input));
+}
+
+TEST(SerializeTests, StoresMultipleWords) 
+{
+    std::vector<std::string> input{"Auto", "Banane", "Bein"};
+    
+    NodeContainer const root = VecToTree(input, 1);
+    ASSERT_THAT(root, SizeIs(2));
+    EXPECT_EQ(root[0].content, 'A');
+    EXPECT_EQ(root[1].content, 'B');
+
+    ASSERT_THAT(root[0].children, SizeIs(1));
+    EXPECT_EQ(root[0].children[0].content, 'u');
+    
+    ASSERT_THAT(root[1].children, SizeIs(2));
+    EXPECT_EQ(root[1].children[0].content, 'a');
+    EXPECT_EQ(root[1].children[1].content, 'e');
+
+    std::vector<std::string> result = TreeToVec("", root);
+    EXPECT_THAT(result, ContainerEq(input));
 }
